bc64424afa7cc5de2ff75f39d7639ddc07721a93
James F Odeh <thenamesviper@gmail.com>
2015-03-05 21:48:52 -0800

    First working version, with saving

--- hangman.rb
+++ hangman.rb
@@ -1,6 +1,8 @@
+require "yaml"
+
 class Hangman
-	attr_accessor :chosen_letters
-	def initialize
+	attr_reader :chosen_letters, :word, :guesses_left, :filtered_word, :game_name
+	def initialize(name)
 		file = File.new("dictionary.txt", "r")
 		dictionary = []
 		file.readlines.each do |line|
@@ -8,6 +10,7 @@ class Hangman
 				dictionary << line
 			end
 		end
+		@game_name = name
 		@guesses_left = 6
 		@word = dictionary[rand(dictionary.length)].chomp.upcase
 		@chosen_letters = []
@@ -15,6 +18,7 @@ class Hangman
 		while @filtered_word.length < @word.length
 			@filtered_word += "_"
 		end
+		puts self.instance_variables
 		play_game
 	end
 	
@@ -25,27 +29,54 @@ class Hangman
 		end
 		puts ""
 		puts "GUESSES LEFT: #{@guesses_left}"
+		puts ""
+		puts "SELECTED LETTERS: #{@chosen_letters.sort.join(" ")}"
 	end
 	
 	def play_game
 			print_board
+			puts self.chosen_letters
 			puts "Please guess a letter"
 			guess = gets.chomp
 			letter = guess.upcase
-			verify_letter(letter)
+			if letter == "SAVE"
+				save_game
+				puts "GAME SAVED"
+				play_game
+			end
+			letter = verify_letter(letter)
 			check_letter(letter)
 	end
 	
+	
+	private
+	def save_game
+		save_data = YAML::dump(self)
+		Dir.mkdir("saves") unless Dir.exists? "saves"
+		File.open("saves/#{game_name}.yaml", "w") { |file| file.write(save_data) }
+	end
+	
 	def verify_letter(letter)
-		until letter =~ /[A-Z]/
+		verified_letter = letter = input_is_letter(letter)
+		verified_letter = letter_not_chosen(verified_letter)
+	end
+	
+	def input_is_letter(input)
+		fixed_letter = input
+		while !(fixed_letter =~ /[A-Z]/)
 			puts "Please select a letter"
-			letter = gets.chomp
+			fixed_letter = gets.chomp.upcase
 		end
-		
-		while @chosen_letters.include? letter
+		fixed_letter
+	end
+	
+	def letter_not_chosen(letter)
+		fixed_letter = letter
+		while @chosen_letters.include? fixed_letter
 			puts "You have already selected that letter. Please select another"
-			letter = gets.chomp
+			fixed_letter = gets.chomp.upcase
 		end
+		fixed_letter = input_is_letter(fixed_letter)
 	end
 
 	def check_letter(letter)
@@ -83,9 +114,28 @@ class Hangman
 		if @guesses_left == 0
 			puts "You have lost. The correct answer was '#{@word}'"
 		else
-			puts "You have won!"
+			print_board
+			puts "'#{@word}' is correct! You have won!"
 		end
 	end
 end
 
-game = Hangman.new
+class Save
+	
+end
+
+def create_game
+	puts "Please enter a game name. If you have a saved game, it will be loaded"
+	game_name = gets.chomp
+	#Load game
+	if File.exist?("saves/#{game_name}.yaml")
+		puts "HI"
+		resume_game = (YAML::load_file("saves/#{game_name}.yaml"))
+		resume_game.play_game
+	else 
+		Hangman.new(game_name)
+	end
+end
+	
+
+create_game
